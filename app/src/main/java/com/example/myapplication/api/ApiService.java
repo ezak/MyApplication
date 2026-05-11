200e4cd10acd9cbea6f9b5ca02a4250aeb55e4fc
Your Name <you@example.com>
2026-05-11 16:55:56 +0300

    changed return and added syncClaims

--- app/src/main/java/com/example/myapplication/api/ApiService.java
+++ app/src/main/java/com/example/myapplication/api/ApiService.java
@@ -1,21 +1,30 @@
 package com.example.myapplication.api;
 
 import com.example.myapplication.api.model.ApiResponse;
+import com.example.myapplication.database.model.Claim;
+
+import java.util.List;
 
 import io.reactivex.rxjava3.core.Observable;
+import io.reactivex.rxjava3.core.Single;
+import retrofit2.http.Body;
 import retrofit2.http.POST;
 
 public interface ApiService {
 
     @POST("your/endpoint/path")
-    Observable<ApiResponse> login();
+    Single<ApiResponse> login();
 
     @POST("your/endpoint/path")
-    Observable<ApiResponse> register();
+    Single<ApiResponse> register();
 
     @POST("/posts")
-    Observable<ApiResponse> fetchData();
+    Single<ApiResponse> fetchData();
 
     @POST("your/endpoint/path")
-    Observable<ApiResponse> postData();
+    Single<ApiResponse> postData();
+
+    @POST("/sync")
+    Single<ApiResponse> syncClaims(@Body List<Claim> claimList);
+
 }
