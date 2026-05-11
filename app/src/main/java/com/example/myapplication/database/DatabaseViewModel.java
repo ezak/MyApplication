7aaf1653ed08aac99c955015d0403dd7a9aeccfb
Your Name <you@example.com>
2026-05-11 14:09:57 +0300

    added claim functions

--- app/src/main/java/com/example/myapplication/database/DatabaseViewModel.java
+++ app/src/main/java/com/example/myapplication/database/DatabaseViewModel.java
@@ -8,6 +8,8 @@ import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 import com.example.myapplication.database.model.Category;
+import com.example.myapplication.database.model.Claim;
+import com.example.myapplication.ui.RepositoryHelper;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -110,6 +112,64 @@ public class DatabaseViewModel extends ViewModel {
 
     public void addToCurrentTabTitles(String currentTabTitle) {
         this.currentTabTitles.add(currentTabTitle);
+    }
+
+    /* Claim Related */
+
+    public void insertClaim(Claim claim) {
+        disposables.add(databaseRepository.insert(claim).subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread()).subscribe(
+                aLong -> {
+                    Log.e(TAG, "insertClaim: " );
+                },
+                exception -> {
+                    Log.e(TAG, "insertClaim: " + exception.getLocalizedMessage() );
+                }));
+    }
+
+    public void updateClaim(Claim claim) {
+        disposables.add(databaseRepository.update(claim).subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread()).subscribe(
+                integer -> {
+                    Log.e(TAG, "updateClaim: " + integer );
+                },
+                throwable -> {
+                    Log.e(TAG, "updateClaim: " + throwable.getLocalizedMessage() );
+                }));
+    }
+
+    public void deleteClaim(Claim claim) {
+        disposables.add(databaseRepository.delete(claim).subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread()).subscribe(
+                integer -> {
+                    Log.e(TAG, "updateClaim: " + integer );
+                },
+                throwable -> {
+                    Log.e(TAG, "updateClaim: " + throwable.getLocalizedMessage() );
+                }));
+    }
+
+    public void getAllClaims() {
+        disposables.add(databaseRepository.getAllClaims().subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread()).subscribe(
+                claims -> {
+                    Log.e(TAG, "getAllClaims: " );
+                },
+                throwable -> {
+                    Log.e(TAG, "getAllClaims: " + throwable.getLocalizedMessage());
+                },
+                () -> {
+                    Log.e(TAG, "getAllClaims: completed"  );
+                }));
+    }
+
+    public void getClaimById(int id) {
+        disposables.add(databaseRepository.getClaimById(id).subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread()).subscribe(
+                claim -> {
+                    Log.e(TAG, "getClaimById: " + claim.getClaimAmount());
+                },
+                throwable -> {
+                    Log.e(TAG, "getClaimById: " + throwable.getLocalizedMessage() );
+                },
+                () -> {
+                    Log.e(TAG, "getClaimById: completed " );
+                }));
     }
 
     @Override
